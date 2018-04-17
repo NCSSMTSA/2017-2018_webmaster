dee34b9c3620ad73f7931eb0b109368ad7a288a1
Alex Cini <arcini8@gmail.com>
2018-04-17 14:58:52 -0400

    continuing on sidebar

---
+++ scripts.js
@@ -0,0 +1,7 @@
+function closeSidebar() {
+  document.getElementById("sidebar").style.display = "block";
+}
+
+function openSidebar() {
+  document.getElementById("sidebar").style.display = "none";
+}
