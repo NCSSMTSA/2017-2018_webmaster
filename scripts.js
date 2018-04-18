0a7c9c80fb29e01c9114c7fc5a12b4d3a5dfbb56
Alex Cini <arcini8@gmail.com>
2018-04-18 12:20:57 -0400

    .

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function closeSidebar() {
   document.getElementById("sidebar").style.display = "none";
-  document.getElementById("barButton").style.display = "block";
+  document.getElementById("hamburgerButton").style.display = "block";
 }
 
 function openSidebar() {
   document.getElementById("sidebar").style.display = "inline";
-  document.getElementById("barButton").style.display = "none";
+  document.getElementById("hamburgerButton").style.display = "none";
 }
