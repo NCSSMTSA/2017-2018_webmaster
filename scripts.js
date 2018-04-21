5065aa6a3617b51e5a24bfda366aa539307a7efe
Alex Cini <arcini8@gmail.com>
2018-04-20 23:38:17 -0400

    trying to make this goddamn sidebar oof

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 function closeSidebar() {
   document.getElementById("sidebar").style.display = "none";
-  document.getElementById("hamburgerButton").style.display = "inline-block";
+  document.getElementById("hamburgerButton").style.visibility = "visible";
 }
 
 function openSidebar() {
   document.getElementById("sidebar").style.display = "inline-block";
-  document.getElementById("hamburgerButton").style.display = "none";
+  document.getElementById("hamburgerButton").style.visibility = "hidden";
 }
